a79a402189a6daf83fc4056bdae0b7a4c44fb4b8
Kooreis <kooreis.official@gmail.com>
2024-10-29 16:37:46 -0500

    This update adds the AddNum method to the MedianFinder class. This method takes an integer as input and adds it to the appropriate heap (maxHeap if the number is less than the maximum element of maxHeap, minHeap otherwise). It then rebalances the heaps if necessary.

--- solution.cs
+++ solution.cs
@@ -1,9 +1,24 @@
-using System;
-using System.Collections.Generic;
+public void AddNum(int num)
+    {
+        if (maxHeap.Count == 0 || num < maxHeap.Max.Item1)
+        {
+            maxHeap.Add(new Tuple<int, int>(num, count++));
+        }
+        else
+        {
+            minHeap.Add(new Tuple<int, int>(num, count++));
+        }
 
-public class MedianFinder
-{
-    private SortedSet<Tuple<int, int>> maxHeap = new SortedSet<Tuple<int, int>>();
-    private SortedSet<Tuple<int, int>> minHeap = new SortedSet<Tuple<int, int>>();
-    private int count = 0;
-}
+        if (maxHeap.Count > minHeap.Count + 1)
+        {
+            var max = maxHeap.Max;
+            maxHeap.Remove(max);
+            minHeap.Add(max);
+        }
+        else if (minHeap.Count > maxHeap.Count)
+        {
+            var min = minHeap.Min;
+            minHeap.Remove(min);
+            maxHeap.Add(min);
+        }
+    }
