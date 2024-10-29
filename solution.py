6bc12b589cf9885dbcc258b706ee59a831980dd7
Kooreis <kooreis.official@gmail.com>
2024-10-29 16:37:46 -0500

    This update adds the 'add_num' method to the MedianFinder class. This method takes a number, pushes it into the 'large' heap, then pushes the smallest element of the 'large' heap into the 'small' heap. If the 'large' heap has fewer elements than the 'small' heap after this operation, it pops the largest element from the 'small' heap and pushes it into the 'large' heap.

--- solution.py
+++ solution.py
@@ -1,5 +1,5 @@
-import heapq
-
-class MedianFinder:
-    def __init__(self):
-        self.heaps = [], []
+def add_num(self, num):
+        small, large = self.heaps
+        heapq.heappush(small, -heapq.heappushpop(large, num))
+        if len(large) < len(small):
+            heapq.heappush(large, -heapq.heappop(small))
