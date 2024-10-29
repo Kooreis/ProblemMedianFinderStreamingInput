3d9fa27ed00a000434c24b07a1212a541f54523c
Kooreis <kooreis.official@gmail.com>
2024-10-29 16:37:46 -0500

    In this update, the 'addNum' method is added. This method adds a number to the 'low' queue, then moves the largest number in 'low' to 'high'. If 'low' has fewer elements than 'high' after this, it moves the smallest number in 'high' back to 'low'.

--- solution.java
+++ solution.java
@@ -1,7 +1,7 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
-public class MedianFinder {
-    private PriorityQueue<Integer> low = new PriorityQueue<>(Collections.reverseOrder());
-    private PriorityQueue<Integer> high = new PriorityQueue<>();
-}
+public void addNum(int num) {
+        low.offer(num);
+        high.offer(low.poll());
+        if (low.size() < high.size()) {
+            low.offer(high.poll());
+        }
+    }
